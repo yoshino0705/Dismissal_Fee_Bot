d8084c8a016f0aa770ca70bf7623228cbf2bbeb1
Yoshino <weihaomouse@gmail.com>
2018-05-13 18:14:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print(event)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
